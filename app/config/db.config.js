55e3bb5de5c330a0a787fa52ffa1b47b246942eb
maldee <maliciousgemba@gmail.com>
2020-12-24 23:42:17 +0700

    secs

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -11,3 +11,17 @@ module.exports = {
         idle: 10000
     }
 };
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "admin",
+//     DB: "heroku_7814f402e2e3662",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
