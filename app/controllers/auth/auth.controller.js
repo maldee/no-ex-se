f4a8cb868524db2bc383060e44014433b7dae2bc
maldee <maliciousgemba@gmail.com>
2021-06-16 11:32:21 +0530

    popups

--- app/controllers/auth/auth.controller.js
+++ app/controllers/auth/auth.controller.js
@@ -9,148 +9,143 @@ var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
 exports.signup = (req, res) => {
+  const requser = {
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+    displayName: req.body.displayName,
+    role: "User",
+  };
 
-   
-
-    const requser = {
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 8),
-        displayName: req.body.displayName,
-        role: "User"
-    };
-
-    // Save User to Database
-    User.create(requser)
-        .then(user => {
-
-            if (req.body.roles) {
-                Role.findAll({
-                    where: {
-                        id: {
-                            [Op.or]: req.body.roles
-                        }
-                    }
-                }).then(roles => {
-                    user.setRoles(roles).then(() => {
-                        res.send({ message: "User registered successfully!" });
-                    });
-                });
-            } else {
-
-                var token = jwt.sign({ id: user.id }, config.secret, {
-                    expiresIn: 86400 // 24 hours
-                });
-
-                // user role = 1
-                user.setRoles([1]).then(() => {
-
-                    const userDetails = {
-                        id: user.id,
-                        email: user.email,
-                        password: user.password,
-                        displayName: user.displayName,
-                        role: user.role,
-                        accessToken: token
-                    }
-
-                    res.status(200).send(userDetails);
-
-                    // res.send({ message: "User registered successfully!" });
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
+  // Save User to Database
+  User.create(requser)
+    .then((user) => {
+      if (req.body.roles) {
+        Role.findAll({
+          where: {
+            id: {
+              [Op.or]: req.body.roles,
+            },
+          },
+        }).then((roles) => {
+          user.setRoles(roles).then(() => {
+            res.send({ message: "User registered successfully!" });
+          });
         });
+      } else {
+        var token = jwt.sign({ id: user.id }, config.secret, {
+          expiresIn: 86400, // 24 hours
+        });
+
+        // user role = 1
+        user.setRoles([1]).then(() => {
+          const userDetails = {
+            id: user.id,
+            email: user.email,
+            password: user.password,
+            displayName: user.displayName,
+            role: user.role,
+            accessToken: token,
+          };
+
+          res.status(200).send(userDetails);
+
+          // res.send({ message: "User registered successfully!" });
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
 
 exports.signin = (req, res) => {
+  const loginUser = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    const loginUser = {
-        email: req.body.email,
-        password: req.body.password,
-    }
+  console.log(req.body.email);
 
-    console.log(req.body.email)
+  User.findAndCountAll({ where: { email: loginUser.email } })
+    .then((user) => {
+      console.log("coco count " + user.count);
+      if (user.count == 0) {
+        return res.status(404).send({ message: "User Not found." });
+      } else {
+        console.log("coco 4 " + JSON.stringify(user.rows[0].role));
 
-    User.findAndCountAll({ where: { email: loginUser.email } })
-        .then(user => {
+        var objectArray = [];
+        for (var i in user.rows) {
+          var d = user.rows[i];
 
-            console.log("coco count " + user.count);
-            if (user.count == 0) {
-                return res.status(404).send({ message: "User Not found." });
-            } else {
+          var results = {
+            id: d.id,
+            email: d.email,
+            password: d.password,
+            displayName: d.displayName,
+            role: d.role,
+            accessToken: d.accessToken,
+          };
+          objectArray.push(results);
+        }
+        console.log("start-" + JSON.stringify(objectArray) + "-end");
 
-                console.log("coco 4 " + JSON.stringify(user.rows[0].role));
+        var passwordIsValid = bcrypt.compareSync(
+          req.body.password,
+          user.rows[0].password
+        );
 
-                var objectArray = [];
-                for (var i in user.rows) {
-                    var d = user.rows[i];
+        if (!passwordIsValid) {
+          return res.status(404).send({
+            message: "Invalid Password!",
+          });
+        }
 
-                    var results = {
-                        id: d.id,
-                        email: d.email,
-                        password: d.password,
-                        displayName: d.displayName,
-                        role: d.role,
-                        accessToken: d.accessToken,
-                    };
-                    objectArray.push(results);
-                }
-                console.log("start-" + JSON.stringify(objectArray) + "-end");
-
-
-
-                var passwordIsValid = bcrypt.compareSync(
-                    req.body.password,
-                    user.rows[0].password
-                );
-
-                if (!passwordIsValid) {
-                    return res.status(404).send({
-                        message: "Invalid Password!"
-                    });
-                }
-
-
-
-                var token = jwt.sign({ id: user.id }, config.secret, {
-                    expiresIn: 86400 // 24 hours
-                });
-
-
-                const userDetails = {
-                    id: results.id,
-                    displayName: results.displayName,
-                    email: results.email,
-                    role: user.rows[0].role,
-                    accessToken: token
-                }
-
-                res.status(200).send(userDetails);
-            }
-
-
-
-            // var authorities = [];
-            // user.getRoles().then(roles => {
-            //     for (let i = 0; i < roles.length; i++) {
-            //         authorities.push("ROLE_" + roles[i].name.toUpperCase());
-            //     }
-
-            //     const results = {
-            //         id: user.id,
-            //         displayName: user.displayName,
-            //         email: user.email,
-            //         roles: authorities,
-            //         accessToken: token
-            //     }
-
-            //     res.status(200).send(results);
-            // });
-
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
+        var token = jwt.sign({ id: user.id }, config.secret, {
+          expiresIn: 86400, // 24 hours
         });
+
+        const userDetails = {
+          id: results.id,
+          displayName: results.displayName,
+          email: results.email,
+          role: user.rows[0].role,
+          accessToken: token,
+        };
+
+        res.status(200).send(userDetails);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+exports.signInWithPopup = (req, res) => {
+  const loginUser = {
+    id: req.body.id,
+    displayName: req.body.displayName,
+    email: req.body.email,
+    photoURL: req.body.photoURL,
+    emailVerified: req.body.emailVerified,
+    role: req.body.role
+  };
+
+  console.log(req.body.email);
+
+  var token = jwt.sign({ id: user.id }, config.secret, {
+    expiresIn: 86400, // 24 hours
+  });
+
+  const userDetails = {
+    uid: loginUser.id,
+    displayName: loginUser.displayName,
+    email: loginUser.email,
+    photoURL: loginUser.photoURL,
+    emailVerified: loginUser.emailVerified,
+    role: loginUser.role,
+    accessToken: token,
+  };
+
+  res.status(200).send(userDetails);
 };
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -19,4 +19,6 @@ module.exports = function(app) {
     );
 
     app.post("/api/auth/signin", controller.signin);
+
+    app.post("/api/auth/signInWithPopup", controller.signInWithPopup);
 };
