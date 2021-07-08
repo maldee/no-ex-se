afac79cad31f54ae4123f29726a3cdc8249b42a4
unknown <maliciousgemba@gmail.com>
2021-07-09 01:41:51 +0530

    bytagsk

--- app/controllers/blog/blog.post.controller.js
+++ app/controllers/blog/blog.post.controller.js
@@ -86,7 +86,7 @@ exports.findAllPosts = (req, res) => {
 };
 
 exports.findAllTags = (req, res) => {
-    BlogPost.findAll({attributes: ['tags'], order: 'random()', limit: 30  })
+    BlogPost.findAll({attributes: ['tags'], limit: 30  })
         .then((data) => {
          
             var totalItemCount = data.length;
