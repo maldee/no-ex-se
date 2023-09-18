6d8366eb9dd57bded0fb2ccaed321527e7788dd0
unknown <maliciousgemba@gmail.com>
2023-09-18 08:07:47 +0530

    render init db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,25 +1,9 @@
 // Heroku Integration
 
-// module.exports = {
-//     HOST: "ec2-3-214-3-162.compute-1.amazonaws.com",
-//     USER: "iiymmtiyzjbffq",
-//     PASSWORD: "8be06651f17d7c58bd9b63beea06e0ab9afd6f76f035b05d819bf1dd5b54a826",
-//     DB: "d4ogp2vt9h70fa",
-//     dialect: "postgres",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
-
-// Localhost Integration
-
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "coco1234",
+    HOST: "dpg-ck3rf7r6fquc73ecvp00-a",
+    USER: "dee",
+    PASSWORD: "uyKaxx5NAbCnK74472a7GsWLqFtUXwX0",
     DB: "deeflow",
     dialect: "postgres",
     pool: {
@@ -29,3 +13,19 @@ module.exports = {
         idle: 10000
     }
 };
+
+// Localhost Integration
+
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "coco1234",
+//     DB: "deeflow",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
