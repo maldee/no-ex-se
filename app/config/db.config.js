e7aa23b27fd9d984b95ded7436fcf1881d82eb37
unknown <maliciousgemba@gmail.com>
2023-09-21 18:46:08 +0530

    railway init

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,12 @@
 // Heroku Integration
 
 module.exports = {
-    HOST: "dpg-ck3rf7r6fquc73ecvp00-a",
-    USER: "dee",
-    PASSWORD: "uyKaxx5NAbCnK74472a7GsWLqFtUXwX0",
-    DB: "deeflow",
+    HOST: "containers-us-west-121.railway.app",
+    USER: "postgres",
+    PASSWORD: "qumlKcpiyzcdCwYdc8Ng",
+    DB: "railway",
     dialect: "postgres",
+    port: "7966",
     pool: {
         max: 5,
         min: 0,
@@ -13,6 +14,22 @@ module.exports = {
         idle: 10000
     }
 };
+
+// render db details
+
+// module.exports = {
+//     HOST: "dpg-ck3rf7r6fquc73ecvp00-a",
+//     USER: "dee",
+//     PASSWORD: "uyKaxx5NAbCnK74472a7GsWLqFtUXwX0",
+//     DB: "deeflow",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
 
 // Localhost Integration
 
