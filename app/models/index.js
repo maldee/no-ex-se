6f5e506edcdaa9b4e342b4c44e4c9393915e7a4f
unknown <maliciousgemba@gmail.com>
2023-09-21 19:02:43 +0530

    railway init03

--- app/models/index.js
+++ app/models/index.js
@@ -3,6 +3,7 @@ const config = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
     config.DB,
+    config.port,
     config.USER,
     config.PASSWORD, {
         host: config.HOST,
